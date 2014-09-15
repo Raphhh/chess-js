f15f77d85bf0bdce024c6390bd54627aeb477a03
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2014-09-15 23:17:21 +0200

    create class Board

---
+++ src/Chess/Board/Board.js
@@ -0,0 +1,72 @@
+var Chess = (function(Chess) {
+    'use strict';
+
+    Chess.Board = Chess.Board || {};
+
+    Chess.Board.Board = (function() {
+
+        var initSquares = function() {
+            for(var x = 0; x < 8; ++x) {
+                this.__internal__.squares.push([]);
+                for(var y = 0; y < 8; ++y) {
+                    this.__internal__.squares[x].push(new Chess.Board.Square(new Chess.Movement.Position(x, y)));
+                }
+            }
+        };
+
+        function Board() {
+            this.__internal__ = {
+                pieces: [],
+                squares: []
+            };
+            initSquares.call(this);
+        }
+
+        Board.prototype.getSquareByPosition = function(position) {
+            if(!this.__internal__.squares[position.getX()]) {
+                throw new Error('Position x (' + position.getX() + ') out of board');
+            }
+            if(!this.__internal__.squares[position.getX()][position.getY()]) {
+                throw new Error('Position y (' + position.getY() + ') out of board');
+            }
+            return this.__internal__.squares[position.getX()][position.getY()];
+        };
+
+        Board.prototype.getPieces = function() {
+            return this.__internal__.pieces;
+        };
+
+        Board.prototype.getPieceByPosition = function(position) {
+            return this.getSquareByPosition(position).getPiece();
+        };
+
+        Board.prototype.initPieces = function(piecesData) {
+            var factory = new Chess.Piece.PieceFactory();
+            for(var i = 0, len = piecesData.length; i < len; ++i) {
+                this.addPiece(
+                    factory.createByData(piecesData[i]),
+                    new Chess.Movement.Position(piecesData[i].position.x, piecesData[i].position.y)
+                );
+            }
+        };
+
+        Board.prototype.addPiece = function(piece, position) {
+            this.changePiecePosition(piece, position);
+            this.__internal__.pieces.push(piece);
+        };
+
+        Board.prototype.changePiecePosition = function(piece, position) {
+            var associator = new Chess.Board.SquareAssociator(
+                piece,
+                this.getSquareByPosition(position)
+            );
+            associator.run();
+        };
+
+        return Board;
+
+    })();
+
+    return Chess;
+
+})(Chess || {});
---
+++ test/Chess/Board/BoardTest.js
@@ -0,0 +1,132 @@
+test("getSquareByPosition with last square", function() {
+
+    var board = new Chess.Board.Board();
+    ok(board.getSquareByPosition(new Chess.Movement.Position(7, 7)) instanceof Chess.Board.Square);
+
+});
+
+test("getSquareByPosition out of board", function() {
+
+    var board = new Chess.Board.Board();
+    throws(
+        function() {
+            board.getSquareByPosition(new Chess.Movement.Position(8, 8));
+        },
+        Error,
+        'Position x (8) out of board'
+    );
+
+});
+
+test("getSquareByPosition with last square", function() {
+
+    var board = new Chess.Board.Board();
+    ok(board.getSquareByPosition(new Chess.Movement.Position(7, 7)) instanceof Chess.Board.Square);
+
+});
+
+test("changePiecePosition", function() {
+
+    var board = new Chess.Board.Board();
+    var oldSquare = board.getSquareByPosition(new Chess.Movement.Position(0, 0));
+    var newSquare = board.getSquareByPosition(new Chess.Movement.Position(1, 1));
+
+    //set the current piece
+    var factory = new Chess.Piece.PieceFactory();
+    var piece = factory.create('pawn', 'white');
+    piece.setSquare(oldSquare);
+    oldSquare.setPiece(piece);
+
+    strictEqual(piece.getSquare(), oldSquare);
+    strictEqual(oldSquare.getPiece(), piece);
+
+    //set the new square of the piece
+    var oldPiece = factory.create('pawn', 'black');
+    newSquare.setPiece(oldPiece);
+    oldPiece.setSquare(newSquare);
+
+    strictEqual(newSquare.getPiece(), oldPiece);
+    strictEqual(oldPiece.getSquare(), newSquare);
+
+    //act
+    board.changePiecePosition(piece, new Chess.Movement.Position(1, 1));
+
+    //assert for the piece and its old square
+    strictEqual(piece.getSquare(), newSquare);
+    strictEqual(oldSquare.getPiece(), null);
+
+    //assert for the square and its old piece
+    strictEqual(newSquare.getPiece(), piece);
+    strictEqual(oldPiece.getSquare(), null);
+
+});
+
+test("addPiece", function() {
+
+    var position = new Chess.Movement.Position(0, 0);
+    var board = new Chess.Board.Board();
+    var square = board.getSquareByPosition(position);
+
+    var factory = new Chess.Piece.PieceFactory();
+    var piece = factory.create('pawn', 'white');
+
+    strictEqual(piece.getSquare(), null);
+    strictEqual(square.getPiece(), null);
+
+    board.addPiece(piece, position);
+
+    strictEqual(piece.getSquare(), square);
+    strictEqual(square.getPiece(), piece);
+
+});
+
+test("getPieces", function() {
+
+    var factory = new Chess.Piece.PieceFactory();
+    var piece = factory.create('pawn', 'white');
+
+    var board = new Chess.Board.Board();
+    deepEqual(board.getPieces(), []);
+
+    board.addPiece(piece, new Chess.Movement.Position(0, 0));
+    deepEqual(board.getPieces(), [piece]);
+
+});
+
+test("getPieceByPosition", function() {
+
+    var position = new Chess.Movement.Position(0, 0);
+    var factory = new Chess.Piece.PieceFactory();
+    var piece = factory.create('pawn', 'white');
+
+    var board = new Chess.Board.Board();
+    strictEqual(board.getPieceByPosition(position), null);
+
+    board.addPiece(piece, position);
+    strictEqual(board.getPieceByPosition(position), piece);
+});
+
+test("initPieces", function() {
+
+    var position = new Chess.Movement.Position(0, 0);
+
+    var board = new Chess.Board.Board();
+    deepEqual(board.getPieceByPosition(position), null);
+
+    board.initPieces([
+        {
+            "type": "pawn",
+            "color": "black",
+            "position": {
+                "x": position.getX(),
+                "y": position.getY()
+            }
+        }
+    ]);
+
+
+    var piece = board.getPieceByPosition(position);
+    ok(piece instanceof Chess.Piece.Type.Pawn);
+    strictEqual(piece.getColor().getValue(), 'black');
+    deepEqual(piece.getSquare().getPosition(), position);
+});
