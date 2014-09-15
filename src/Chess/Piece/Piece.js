701b92dde8bd27692c14819e8144f39a77275108
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2014-09-15 23:15:44 +0200

    create class Piece

---
+++ src/Chess/Piece/Piece.js
@@ -0,0 +1,45 @@
+var Chess = (function(Chess) {
+    'use strict';
+
+    Chess.Piece = Chess.Piece || {};
+
+    Chess.Piece.Piece = (function() {
+
+        function Piece(color) {
+            this.__internal__ = {
+                color: color,
+                square: null
+            };
+        }
+
+        Piece.prototype.getName = function() {
+            return '';
+        };
+
+        Piece.prototype.getColor = function() {
+            return this.__internal__.color;
+        };
+
+        Piece.prototype.getSquare = function() {
+            return this.__internal__.square;
+        };
+
+        Piece.prototype.setSquare = function(square) {
+            this.__internal__.square = square;
+        };
+
+        Piece.prototype.die = function() {
+            this.__internal__.square = null;
+        };
+
+        Piece.prototype.getDisplacementType = function() {
+            return null;
+        };
+
+        return Piece;
+
+    })();
+
+    return Chess;
+
+})(Chess || {});
---
+++ test/Chess/Piece/PieceTest.js
@@ -0,0 +1,56 @@
+test("getColor", function() {
+
+    var color1 = 'a';
+    var color2 = 'b';
+
+    var piece1 = new Chess.Piece.Piece(color1);
+    var piece2 = new Chess.Piece.Piece(color2);
+
+    strictEqual(piece1.getColor(), color1);
+    strictEqual(piece2.getColor(), color2);
+});
+
+test("getName", function() {
+
+    var piece = new Chess.Piece.Piece(null);
+
+    strictEqual(piece.getName(), '');
+});
+
+test("getSquare before setSquare", function() {
+
+    var piece = new Chess.Piece.Piece();
+
+    strictEqual(piece.getSquare(), null);
+});
+
+test("getSquare after setSquare", function() {
+
+    var square = 'a';
+
+    var piece = new Chess.Piece.Piece();
+    piece.setSquare(square);
+
+    strictEqual(piece.getSquare(), square);
+});
+
+test("getSquare after die", function() {
+
+    var square = 'a';
+
+    var piece = new Chess.Piece.Piece();
+    piece.setSquare(square);
+
+    strictEqual(piece.getSquare(), square);
+
+    piece.die();
+
+    strictEqual(piece.getSquare(), null);
+});
+
+test("getDisplacementType", function() {
+
+    var piece = new Chess.Piece.Piece(null);
+
+    strictEqual(piece.getDisplacementType(), null);
+});
