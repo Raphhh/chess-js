907fc8a7cf616c574948994f4048306d400da796
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2014-09-15 23:13:40 +0200

    create class Chess

---
+++ src/Chess.js
@@ -0,0 +1,10 @@
+var Chess = (function() {
+    'use strict';
+
+    function Chess() {
+    }
+
+    Chess.VERSION = '0.0.0';
+
+    return Chess;
+})();
---
+++ test/Chess/ChessTest.js
@@ -0,0 +1,3 @@
+test("Chess", function() {
+    ok(Chess);
+});
